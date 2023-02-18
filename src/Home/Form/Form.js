089fa9e123506042d702a7a187c02eec8a71b0ec
phcatanduba@gmail.com <phcatanduba@gmail.com>
2023-02-18 17:37:59 -0300

    adiciona inputs + responsividade

--- src/Home/Form/Form.js
+++ src/Home/Form/Form.js
@@ -1,9 +1,14 @@
+import LoginInput from "../../Components/LoginInput/LoginInput";
 import "./Form.css"
 
 function Form() {
     return(
         <div className="body">
             <div className="blur" />
+            <form>
+                <LoginInput placeholder={"Usuário"} type="name"/>
+                <LoginInput placeholder={"Senha"} type="password"/>
+            </form>
         </div>
     );
 }
---
+++ src/Home/RememberMe/RememberMe.js
@@ -0,0 +1,11 @@
+function RememberMe() {
+    return (
+        <>
+            <input type="checkbox" id="checkbox" value={true} />
+            <label for="checkbox">Lembrar de mim</label>
+        </>
+    );
+}
+
+export default RememberMe;
+
