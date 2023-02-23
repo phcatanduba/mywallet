4fab99677d86cb5ecb5c690663ceccea08b0fad3
Anderson Leonardo <andersonleonardo.sapinto@gmail.com>
2023-02-23 12:12:41 -0300

    Mostrar senha login e instalação de libs

--- src/Home/Form/Form.js
+++ src/Home/Form/Form.js
@@ -5,15 +5,48 @@ import ForgotPassword from "./ForgotPassword/ForgotPassword";
 import "./Form.css"
 import FormStrings from "./Strings";
 import SignUpLink from "./SignUpLink/SignUpLink";
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import Visibility from "@material-ui/icons/Visibility";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
 
 function Form() {
+    const [values, setValues] = React.useState({
+        password: "",
+        showPassword: false,
+      });
+      
+      const handleClickShowPassword = () => {
+        setValues({ ...values, showPassword: !values.showPassword });
+      };
+      
+      const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+      };
+      
+      const handlePasswordChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+      };
     return(
         <div className="container">
             <div className="blur" />
             <div className="form-container">
                 <form>
                     <LoginInput placeholder={FormStrings.userPlaceholder} type="name"/>
-                    <LoginInput placeholder={FormStrings.passwordPlaceholder} type="password"/>
+                    <LoginInput placeholder={FormStrings.passwordPlaceholder} type={values.showPassword ? "text" : "password"}
+                        onChange={handlePasswordChange("password")}
+                        value={values.password}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                >
+                                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                                </IconButton>
+                            </InputAdornment>
+                        }/>
                     <div className="box-form">
                         <RememberMe />
                         <ForgotPassword />
