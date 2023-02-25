b402db9be09ec2c001e2397210b38a1b0b38218f
Anderson Leonardo <andersonleonardo.sapinto@gmail.com>
2023-02-25 18:39:19 -0300

    criação do schema

---
+++ src/Components/SignUp/schemasValidation.js
@@ -0,0 +1,27 @@
+import * as yup from "yup";
+
+const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
+// min 5 characters, 1 upper case letter, 1 lower case letter, 1 numeric digit.
+
+export const schemasForm = yup.object().shape({
+    name: yup
+        .string()
+        .required("Digite seu nome"),
+    email: yup
+        .string()
+        .email("Preencha com um email valido")
+        .required("Preencha seu email"),
+    cpf: yup
+        .number()
+        .max(11)
+        .required("Preencha seu CPF"),
+    password: yup
+        .string()
+        .min(6)
+        .matches(passwordRules, { message: "Please create a stronger password" })
+        .required("Required"),
+    confirmPassword: yup
+        .string()
+        .oneOf([yup.ref("password"), null], "As senhas devem ser iguais")
+        .required("Required"),
+});
